c9f141ed9b0830c4568292602673cd2b113200ab
ashwindevx <ashwindevx@gmail.com>
2022-12-30 18:07:52 +0530

    firebase setup with login/logout working

---
+++ src/firebase.tsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBKZF7Zb9VROWsRl-HmiCQg-F1zsd1MQlY",
+  authDomain: "year-old.firebaseapp.com",
+  projectId: "year-old",
+  storageBucket: "year-old.appspot.com",
+  messagingSenderId: "947016283031",
+  appId: "1:947016283031:web:895289a79f8bc103eccad2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
